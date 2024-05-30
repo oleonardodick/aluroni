9ce4437eac956262ddb15b04faaa97bce48811c9
Leonardo Dick Bernardes <leonardod.bernardes@gmail.com>
2024-05-30 16:12:04 -0300

    Subindo o projeto ao Github

---
+++ src/pages/Prato/index.tsx
@@ -0,0 +1,39 @@
+import styles from './Prato.module.scss';
+import { useParams, useNavigate } from 'react-router-dom';
+import cardapio from '../../data/cardapio.json';
+import { lazy } from 'react';
+
+const TagsPrato = lazy(() => import('components/TagsPrato'));
+const NotFound = lazy(() => import('pages/NotFound'));
+const PaginaPadrao = lazy(() => import('components/PaginaPadrao'));
+
+export default function Prato() {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const prato = cardapio.find((item) => item.id === Number(id));
+  if (!prato) {
+    return <NotFound />;
+  }
+  return (
+    <PaginaPadrao>
+      <button
+        className={styles.voltar}
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        {'< Voltar'}
+      </button>
+      <section className={styles.container}>
+        <h1 className={styles.titulo}>{prato.title}</h1>
+        <div className={styles.imagem}>
+          <img src={prato.photo} alt={prato.title} />
+        </div>
+        <div className={styles.conteudo}>
+          <p className={styles.conteudo__descricao}>{prato.description}</p>
+          <TagsPrato {...prato} />
+        </div>
+      </section>
+    </PaginaPadrao>
+  );
+}
